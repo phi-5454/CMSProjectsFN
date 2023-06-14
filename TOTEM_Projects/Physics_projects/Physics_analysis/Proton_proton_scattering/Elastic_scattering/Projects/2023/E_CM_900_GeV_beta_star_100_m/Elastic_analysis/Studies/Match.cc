96c78614eff6efecb3cfbf9511c5ed54f930d36f
Frici <fnemes@cern.ch>
2023-06-14 14:24:15 +0200

    Remove useless files

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Match.cc
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Match.cc
@@ -549,194 +549,209 @@ void plot_contour()
 
 int main()
 {
+  const int main_scenario_plot_fiducial_cuts = 1 ;
+  const int main_scenario_plot_apertures = 2 ;
+  const int main_scenario_optics_test = 3 ;
 
-  plot_contour() ;
-  
-  exit(1) ;
-
+  int main_scenario = main_scenario_plot_apertures ;
 
   t_GeV2_distribution = new TF1("t_GeV2_distribution", my_exponential_distribution, 0.0, 7.0, 2) ;
 
   // gStyle->SetOptStat(0) ;
   // gStyle->SetOptFit(1111) ;
 
-	TF1 *t_GeV2_distribution = new TF1("t_GeV2_distribution", my_exponential_distribution, 0.0, 7.0, 2) ;
+  TF1 *t_GeV2_distribution = new TF1("t_GeV2_distribution", my_exponential_distribution, 0.0, 7.0, 2) ;
   t_GeV2_distribution->SetParameters(Constant, Slope) ;
   t_GeV2_distribution->SetNpx(100000) ;
-  
-//   TH1D *hist_minus_t_GeV2 = new TH1D("hist_minus_t_GeV2", "hist_minus_t_GeV2", 100, 0, 2) ;
-//   TH1D *hist_minus_t_GeV2_reco = new TH1D("hist_minus_t_GeV2_reco", "hist_minus_t_GeV2_reco", 100, 0, 2) ;
-//   TH1D *hist_minus_t_GeV2_reco_diff_rel_percent = new TH1D("hist_minus_t_GeV2_reco_diff_rel_percent", "hist_minus_t_GeV2_reco_diff_rel_percent", 100, 1e-2, -1e-2) ;
+
+  //   TH1D *hist_minus_t_GeV2 = new TH1D("hist_minus_t_GeV2", "hist_minus_t_GeV2", 100, 0, 2) ;
+  //   TH1D *hist_minus_t_GeV2_reco = new TH1D("hist_minus_t_GeV2_reco", "hist_minus_t_GeV2_reco", 100, 0, 2) ;
+  //   TH1D *hist_minus_t_GeV2_reco_diff_rel_percent = new TH1D("hist_minus_t_GeV2_reco_diff_rel_percent", "hist_minus_t_GeV2_reco_diff_rel_percent", 100, 1e-2, -1e-2) ;
 
   TH1D *hist_minus_t_GeV2 = new TH1D("hist_minus_t_GeV2", "hist_minus_t_GeV2", 100, 0, 2) ;
   TH1D *hist_minus_t_GeV2_reco = new TH1D("hist_minus_t_GeV2_reco", "hist_minus_t_GeV2_reco", 100, 0, 2) ;
 
   TH1D *hist_minus_t_GeV2_reco_diff_rel_percent = new TH1D("hist_minus_t_GeV2_reco_diff_rel_percent", "hist_minus_t_GeV2_reco_diff_rel_percent", 100, 1e-2, -1e-2) ;
 
-	TH2D *hist = new TH2D("hist", "hist", 100, -5e+2, 5e+2, 100, -1000, 1000) ;
+  TH2D *hist = new TH2D("hist", "hist", 100, -5e+2, 5e+2, 100, -1000, 1000) ;
 
-	TH2D *hist_compare_theta_y_star = new TH2D("hist_compare_theta_y_star", "hist_compare_theta_y_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
-	TH2D *hist_compare_y_star = new TH2D("hist_compare_y_star", "hist_compare_y_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
+  TH2D *hist_compare_theta_y_star = new TH2D("hist_compare_theta_y_star", "hist_compare_theta_y_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
+  TH2D *hist_compare_y_star = new TH2D("hist_compare_y_star", "hist_compare_y_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
 
-	TH2D *hist_compare_theta_x_star = new TH2D("hist_compare_theta_x_star", "hist_compare_theta_x_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
-	TH2D *hist_compare_x_star = new TH2D("hist_compare_x_star", "hist_compare_x_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
+  TH2D *hist_compare_theta_x_star = new TH2D("hist_compare_theta_x_star", "hist_compare_theta_x_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
+  TH2D *hist_compare_x_star = new TH2D("hist_compare_x_star", "hist_compare_x_star", 100, -16.0e-4, 16.0e-4,  100, -16.0e-4, 16.0e-4) ;
 
-	TH2D *hist_theta_x_y_star_reco = new TH2D("hist_theta_x_y_star_reco", "hist_theta_x_y_star_reco", 100, -1000e-6, 1000e-6, 100, -1000e-6, 1000e-6) ;
+  TH2D *hist_theta_x_y_star_reco = new TH2D("hist_theta_x_y_star_reco", "hist_theta_x_y_star_reco", 100, -1000e-6, 1000e-6, 100, -1000e-6, 1000e-6) ;
 
-	// Best effect
-	const double delta_n = -2.0 * 0.0 ;
-	const double delta_f = -3.0 * 0.0 ;
+  // Best effect
+  const double delta_n = -2.0 * 0.0 ;
+  const double delta_f = -3.0 * 0.0 ;
 
-	// 
+  // 
 
   const double Lx_near_reco = Lx_near + delta_n ;
   const double Lx_far_reco = Lx_far   + delta_f ;
 
   const double Ly_near_reco = Ly_near + delta_n ;
   const double Ly_far_reco = Ly_far   + delta_f ;
-
-  read_apertures() ;
   
-  exit(1) ;
 
-	for(int i = 0 ; i < 1e6 ; ++i)
-	{
+  if(main_scenario == main_scenario_plot_fiducial_cuts)
+  {
+    plot_contour() ;
+  }
+  else if(main_scenario == main_scenario_plot_apertures)
+  {
 
-    const double minus_t_GeV2 = t_GeV2_distribution->GetRandom() ;
-    const Double_t phi_IP5_rad = gRandom->Uniform(0, PI_TIMES_2) ;    
+    read_apertures() ;
+
+  }
+  else if(main_scenario == main_scenario_optics_test)
+  {
+
+    for(int i = 0 ; i < 1e6 ; ++i)
+    {
+
+      const double minus_t_GeV2 = t_GeV2_distribution->GetRandom() ;
+      const Double_t phi_IP5_rad = gRandom->Uniform(0, PI_TIMES_2) ;    
+
+      double theta_star_rad = theta_star_rad_from_t_GeV2(-minus_t_GeV2, beam_momentum_GeV) ;
+
+      double theta_x_star = (theta_star_rad * cos(phi_IP5_rad)) ;
+      double theta_y_star = (theta_star_rad * sin(phi_IP5_rad)) ;
+
+	    double y_star = 1.0 * 200e-6 * myrand.Gaus() ;
+	    double x_star = 1.0 * 200e-6 * myrand.Gaus() ;
+
+	    double beam_divergence_x = 0.0 * 20e-6 * myrand.Gaus() ;
+	    double beam_divergence_y = 0.0 * 20e-6 * myrand.Gaus() ;
+
+	    double theta_x_star_pert = theta_x_star + beam_divergence_x ;
+	    double theta_y_star_pert = theta_y_star + beam_divergence_y ;
+
+	    double x_near = (Lx_near * theta_x_star_pert) + (vx_near * x_star) ;
+	    double x_far =  (Lx_far *  theta_x_star_pert) + (vx_far *  x_star) ;
+	    double y_near = (Ly_near * theta_y_star_pert) + (vy_near * y_star) ;
+	    double y_far =  (Ly_far *  theta_y_star_pert) + (vy_far *  y_star) ;
+
+	    // cout << y_far << endl ;
+    //		if(fabs(x_far)< 3e-3)
+    //		if(fabs(x_near)< 3e-3)
+
+	    // if((y_far < -1.e-3) && (y_far > -10.0e-3) )
+      //	if(fabs(x_near) < 3e-3)
+      //	if(fabs(x_far) < 3e-3)
+      //	if((y_far < -4.0e-3) && (y_far > -10.0e-3) && (y_near < (-4.0e-3*1.4*(Ly_far/Ly_near))) && (y_near > (-10.0e-3*(Ly_far/Ly_near))))
+	    {
+
+        double determinant_x = ((Lx_near_reco * vx_far) - (Lx_far_reco * vx_near)) ;
+        double determinant_y = ((Ly_near_reco * vy_far) - (Ly_far_reco * vy_near)) ;
+
+		    double theta_y_star_reco = ((y_near * vy_far) - (y_far * vy_near)) / determinant_y ;
+		    // double theta_y_star_reco = ((y_near / Ly_near_reco) + (y_far / Ly_far_reco)) / 2.0 ;
+		    double y_star_reco =      -((y_near * Ly_far_reco) - (y_far * Ly_near_reco)) / determinant_y ;
+
+		    double theta_x_star_reco = ((x_near * vx_far) - (x_far * vx_near)) / determinant_x ;
+		    double x_star_reco =      -((x_near * Lx_far_reco) - (x_far * Lx_near_reco)) / determinant_x ;
+
+        double theta_star_reco = sqrt((theta_x_star_reco*theta_x_star_reco) + (theta_y_star_reco*theta_y_star_reco)) ;
+        double minus_t_GeV2_reco = - beam_momentum_GeV * beam_momentum_GeV * theta_star_reco * theta_star_reco ;
+
+        hist_minus_t_GeV2->Fill(minus_t_GeV2) ;
+        hist_minus_t_GeV2_reco->Fill(-minus_t_GeV2_reco) ;
+
+        hist_minus_t_GeV2_reco_diff_rel_percent->Fill(((minus_t_GeV2 + minus_t_GeV2_reco) / minus_t_GeV2) * 100.0) ;
+
+		    hist_compare_theta_x_star->Fill(theta_x_star_pert, theta_x_star_reco) ;
+		    hist_compare_x_star->Fill(x_star, x_star_reco) ;
+
+		    hist_compare_theta_y_star->Fill(theta_y_star_pert, theta_y_star_reco) ;
+		    hist_compare_y_star->Fill(y_star, y_star_reco) ;
+
+        hist_theta_x_y_star_reco->Fill(theta_x_star_reco, theta_y_star_reco) ;
+
+		    hist->Fill(theta_y_star_reco * 1e6, y_star_reco * 1e6) ;
+	    }
+
+    }
+
+    gStyle->SetPalette(1) ;
+
+    TCanvas c ;
+
+    gStyle->SetTitleFillColor(kWhite) ;
+    gStyle->SetStatColor(kWhite) ;
+    gStyle->SetFillColor(kWhite) ;
+
+    gStyle->SetPadGridX(kTRUE);
+    gStyle->SetPadGridY(kTRUE);
+
+    hist->SetFillColor(kWhite) ;
+    hist->SetTitle("") ;
+
+    hist->Draw("colz") ;
+
+    hist->GetXaxis()->SetTitle("#theta_{x,reco}* [#murad]") ;
+    hist->GetYaxis()->SetTitle("x_{reco}* [#mum]") ;
+    hist->GetYaxis()->SetTitleOffset(1.2) ;
+
+    TLatex *latex = new TLatex() ;
+    latex->SetTextFont(132) ;
+    latex->SetNDC();
+    latex->DrawText(0.24, 0.92, "simulation preliminary") ;
+
+    latex->SetTextFont(22) ;
+    latex->DrawText(0.12, 0.92, "TOTEM") ;
+    latex->Draw("same") ;
+
+
+    c.SetFillColor(kWhite) ;
+
+    //	gStyle->SetOptStat(0);
+
+    c.SetGridx() ;
+    c.SetGridy() ;	
+
+    hist->GetXaxis()->SetAxisColor(17);
+    hist->GetYaxis()->SetAxisColor(17);	
+
+    c.SetLogz() ;
+    c.SaveAs("plots/hist.pdf") ;
+
+    hist_compare_theta_x_star->Draw("colz") ;
+    c.SaveAs("plots/hist_compare_theta_x_star.pdf") ;
+
+    hist_compare_x_star->Draw("colz") ;
+    c.SaveAs("plots/hist_compare_x_star.pdf") ;
+
+    hist_compare_theta_y_star->Draw("colz") ;
+    c.SaveAs("plots/hist_compare_theta_y_star.pdf") ;
+
+    hist_compare_y_star->Draw("colz") ;
+    c.SaveAs("plots/hist_compare_y_star.pdf") ;
+
+    hist_theta_x_y_star_reco->Draw("colz") ;
+    c.SaveAs("plots/hist_theta_x_y_star_reco.pdf") ;
+    c.SaveAs("plots/hist_theta_x_y_star_reco.root") ;
+
+    // cout >> hist_minus_t_GeV2->Integral() ;
+    hist_minus_t_GeV2->Draw("colz") ;
+    hist_minus_t_GeV2->GetYaxis()->SetRangeUser(0.1, 1e7) ;
+    c.SetLogy() ;
+    c.SaveAs("plots/hist_minus_t_GeV2.pdf") ;
+
+    hist_minus_t_GeV2_reco->Draw("colz") ;
+    hist_minus_t_GeV2_reco->GetYaxis()->SetRangeUser(0.1, 1e7) ;
+    c.SetLogy() ;
+    c.SaveAs("plots/hist_minus_t_GeV2_reco.pdf") ;
+
+    hist_minus_t_GeV2_reco_diff_rel_percent->Draw("colz") ;
+    c.SetLogy(0) ;
+    c.SaveAs("plots/hist_minus_t_GeV2_reco_diff_rel_percent.pdf") ;
   
-    double theta_star_rad = theta_star_rad_from_t_GeV2(-minus_t_GeV2, beam_momentum_GeV) ;
-
-    double theta_x_star = (theta_star_rad * cos(phi_IP5_rad)) ;
-    double theta_y_star = (theta_star_rad * sin(phi_IP5_rad)) ;
-  
-	  double y_star = 1.0 * 200e-6 * myrand.Gaus() ;
-	  double x_star = 1.0 * 200e-6 * myrand.Gaus() ;
-
-		double beam_divergence_x = 0.0 * 20e-6 * myrand.Gaus() ;
-		double beam_divergence_y = 0.0 * 20e-6 * myrand.Gaus() ;
-		
-		double theta_x_star_pert = theta_x_star + beam_divergence_x ;
-		double theta_y_star_pert = theta_y_star + beam_divergence_y ;
-
-		double x_near = (Lx_near * theta_x_star_pert) + (vx_near * x_star) ;
-		double x_far =  (Lx_far *  theta_x_star_pert) + (vx_far *  x_star) ;
-		double y_near = (Ly_near * theta_y_star_pert) + (vy_near * y_star) ;
-		double y_far =  (Ly_far *  theta_y_star_pert) + (vy_far *  y_star) ;
-
-		// cout << y_far << endl ;
-//		if(fabs(x_far)< 3e-3)
-//		if(fabs(x_near)< 3e-3)
-
-		// if((y_far < -1.e-3) && (y_far > -10.0e-3) )
-  	//	if(fabs(x_near) < 3e-3)
-  	//	if(fabs(x_far) < 3e-3)
-  	//	if((y_far < -4.0e-3) && (y_far > -10.0e-3) && (y_near < (-4.0e-3*1.4*(Ly_far/Ly_near))) && (y_near > (-10.0e-3*(Ly_far/Ly_near))))
-		{
-
-      double determinant_x = ((Lx_near_reco * vx_far) - (Lx_far_reco * vx_near)) ;
-      double determinant_y = ((Ly_near_reco * vy_far) - (Ly_far_reco * vy_near)) ;
-
-			double theta_y_star_reco = ((y_near * vy_far) - (y_far * vy_near)) / determinant_y ;
-			// double theta_y_star_reco = ((y_near / Ly_near_reco) + (y_far / Ly_far_reco)) / 2.0 ;
-			double y_star_reco =      -((y_near * Ly_far_reco) - (y_far * Ly_near_reco)) / determinant_y ;
-
-			double theta_x_star_reco = ((x_near * vx_far) - (x_far * vx_near)) / determinant_x ;
-			double x_star_reco =      -((x_near * Lx_far_reco) - (x_far * Lx_near_reco)) / determinant_x ;
-      
-      double theta_star_reco = sqrt((theta_x_star_reco*theta_x_star_reco) + (theta_y_star_reco*theta_y_star_reco)) ;
-      double minus_t_GeV2_reco = - beam_momentum_GeV * beam_momentum_GeV * theta_star_reco * theta_star_reco ;
-      
-      hist_minus_t_GeV2->Fill(minus_t_GeV2) ;
-      hist_minus_t_GeV2_reco->Fill(-minus_t_GeV2_reco) ;
-      
-      hist_minus_t_GeV2_reco_diff_rel_percent->Fill(((minus_t_GeV2 + minus_t_GeV2_reco) / minus_t_GeV2) * 100.0) ;
-
-			hist_compare_theta_x_star->Fill(theta_x_star_pert, theta_x_star_reco) ;
-			hist_compare_x_star->Fill(x_star, x_star_reco) ;
-
-			hist_compare_theta_y_star->Fill(theta_y_star_pert, theta_y_star_reco) ;
-			hist_compare_y_star->Fill(y_star, y_star_reco) ;
-      
-      hist_theta_x_y_star_reco->Fill(theta_x_star_reco, theta_y_star_reco) ;
-
-			hist->Fill(theta_y_star_reco * 1e6, y_star_reco * 1e6) ;
-		}
-
-	}
-	
-	gStyle->SetPalette(1) ;
-
-	TCanvas c ;
-	
-  gStyle->SetTitleFillColor(kWhite) ;
-  gStyle->SetStatColor(kWhite) ;
-  gStyle->SetFillColor(kWhite) ;
-
-	gStyle->SetPadGridX(kTRUE);
-	gStyle->SetPadGridY(kTRUE);
-	
-	hist->SetFillColor(kWhite) ;
-	hist->SetTitle("") ;
-
-	hist->Draw("colz") ;
-	
-	hist->GetXaxis()->SetTitle("#theta_{x,reco}* [#murad]") ;
-	hist->GetYaxis()->SetTitle("x_{reco}* [#mum]") ;
-	hist->GetYaxis()->SetTitleOffset(1.2) ;
-
-	TLatex *latex = new TLatex() ;
-	latex->SetTextFont(132) ;
-	latex->SetNDC();
-	latex->DrawText(0.24, 0.92, "simulation preliminary") ;
-
-	latex->SetTextFont(22) ;
-	latex->DrawText(0.12, 0.92, "TOTEM") ;
-	latex->Draw("same") ;
-	
-	
-	c.SetFillColor(kWhite) ;
-	
-//	gStyle->SetOptStat(0);
-
-  c.SetGridx() ;
-  c.SetGridy() ;	
-	
-	hist->GetXaxis()->SetAxisColor(17);
-	hist->GetYaxis()->SetAxisColor(17);	
-
-	c.SetLogz() ;
-	c.SaveAs("plots/hist.pdf") ;
-	
-	hist_compare_theta_x_star->Draw("colz") ;
-	c.SaveAs("plots/hist_compare_theta_x_star.pdf") ;
-
-	hist_compare_x_star->Draw("colz") ;
-	c.SaveAs("plots/hist_compare_x_star.pdf") ;
-
-	hist_compare_theta_y_star->Draw("colz") ;
-	c.SaveAs("plots/hist_compare_theta_y_star.pdf") ;
-
-	hist_compare_y_star->Draw("colz") ;
-	c.SaveAs("plots/hist_compare_y_star.pdf") ;
-
-  hist_theta_x_y_star_reco->Draw("colz") ;
-	c.SaveAs("plots/hist_theta_x_y_star_reco.pdf") ;
-	c.SaveAs("plots/hist_theta_x_y_star_reco.root") ;
-
-  // cout >> hist_minus_t_GeV2->Integral() ;
-  hist_minus_t_GeV2->Draw("colz") ;
-  hist_minus_t_GeV2->GetYaxis()->SetRangeUser(0.1, 1e7) ;
-  c.SetLogy() ;
-	c.SaveAs("plots/hist_minus_t_GeV2.pdf") ;
-
-  hist_minus_t_GeV2_reco->Draw("colz") ;
-  hist_minus_t_GeV2_reco->GetYaxis()->SetRangeUser(0.1, 1e7) ;
-  c.SetLogy() ;
-	c.SaveAs("plots/hist_minus_t_GeV2_reco.pdf") ;
-  
-  hist_minus_t_GeV2_reco_diff_rel_percent->Draw("colz") ;
-  c.SetLogy(0) ;
-	c.SaveAs("plots/hist_minus_t_GeV2_reco_diff_rel_percent.pdf") ;
-  
+  }
+  else
+  {
+    cout << "Unknown scenario: " << main_scenario << endl ;
+  }
 }	
