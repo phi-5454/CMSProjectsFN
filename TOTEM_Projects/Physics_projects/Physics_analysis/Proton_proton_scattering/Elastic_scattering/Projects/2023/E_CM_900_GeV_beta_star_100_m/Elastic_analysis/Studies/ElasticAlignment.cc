39fa93dc4b23f142ad163cba48f32384ca64ec59
Frici <fnemes@cern.ch>
2023-08-14 15:33:42 +0200

    Elastic alignement

---
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/ElasticAlignment.cc
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "math.h"
+#include "math.h"
+
+using namespace std;
+
+#include "stdlib.h"
+#include "TF1.h"
+#include "TGraph.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TProfile.h"
+#include "TFile.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TGraphErrors.h"
+#include "TLatex.h"
+#include "TLine.h"
+#include "TRandom3.h"
+#include "TMinuit.h"
+#include "TStyle.h"
+#include "TFitResult.h"
+
+TH2D *flipHistogram(TH2D *histogram)
+{
+	int xbins = histogram->GetXaxis()->GetNbins() ;
+	int ybins = histogram->GetYaxis()->GetNbins() ;
+
+	int x_axis_min = histogram->GetXaxis()->GetXmin() ;
+	int x_axis_max = histogram->GetXaxis()->GetXmax() ;
+
+	int y_axis_min = histogram->GetYaxis()->GetXmin() ;
+	int y_axis_max = histogram->GetYaxis()->GetXmax() ;
+
+	TH2D *histogram_rotated = new TH2D("test", "test", ybins, y_axis_min, y_axis_max, xbins, x_axis_min, x_axis_max) ;
+	
+	for(int i = 0 ; i < xbins ; ++i)
+	for(int j = 0 ; j < ybins ; ++j)
+	{
+		double bincontent = histogram->GetBinContent(i, j) ;
+		double bin_unc = histogram->GetBinError(i, j) ;
+	
+		histogram_rotated->SetBinContent(j, i, bincontent) ;
+		histogram_rotated->SetBinError(j, i, bin_unc) ;
+	}
+	
+	// histogram_rotated->SaveAs("histogram_rotated.root") ;
+	
+	return histogram_rotated ;
+}
+
+
+/*
+main()
+{
+	gStyle->SetLineScalePS(.3) ;
+	gStyle->SetOptFit(1111);
+
+	TFile *file_LBRT = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts/Generic.root") ;
+	TFile *file_LTRB = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts/Generic.root") ;
+
+	TH2D *x_mm_y_mm_near_left_aligned_LBRT = ((TH2D *)(file_LBRT->Get("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned"))) ;
+	TH2D *x_mm_y_mm_near_left_aligned_LTRB = ((TH2D *)(file_LTRB->Get("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned"))) ;
+
+//	TH2D *x_mm_y_mm_near_left_aligned_LBRT = ((TH2D *)(file_LBRT->Get("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned"))) ;
+//	TH2D *x_mm_y_mm_near_left_aligned_LTRB = ((TH2D *)(file_LTRB->Get("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned"))) ;
+
+//	TH2D *x_mm_y_mm_near_left_aligned_LBRT = ((TH2D *)(file_LBRT->Get("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned"))) ;
+//	TH2D *x_mm_y_mm_near_left_aligned_LTRB = ((TH2D *)(file_LTRB->Get("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned"))) ;
+
+//	TH2D *x_mm_y_mm_near_left_aligned_LBRT = ((TH2D *)(file_LBRT->Get("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned"))) ;
+//	TH2D *x_mm_y_mm_near_left_aligned_LTRB = ((TH2D *)(file_LTRB->Get("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned"))) ;
+	
+	TH2D *x_mm_y_mm_near_left_aligned_combined = (TH2D *)x_mm_y_mm_near_left_aligned_LBRT->Clone("x_mm_y_mm_near_left_aligned_LBRT_combined") ;
+	x_mm_y_mm_near_left_aligned_combined->Add(x_mm_y_mm_near_left_aligned_LTRB) ;
+	TH2D *x_mm_y_mm_near_left_aligned_combined_rotated = flipHistogram(x_mm_y_mm_near_left_aligned_combined) ;
+	
+	c.SetLogz();
+	c.SetGridx() ;	
+	c.SetGridy() ;	
+
+	x_mm_y_mm_near_left_aligned_combined_rotated->SaveAs("plots/ElasticAlignment/x_mm_y_mm_near_left_aligned_combined.root") ;
+	TProfile *x_mm_y_mm_near_left_aligned_combined_rotated_profile = x_mm_y_mm_near_left_aligned_combined_rotated->ProfileX("prof_1") ;
+	
+	x_mm_y_mm_near_left_aligned_combined_rotated_profile->Fit("pol1", "S", "", -10, 10) ;
+	double misalignment_x_mm = x_mm_y_mm_near_left_aligned_combined_rotated_profile->GetFunction("pol1")->Eval(0.0) ;
+	cout << "misalignment_x_mm: " << misalignment_x_mm << endl ;
+	
+	x_mm_y_mm_near_left_aligned_combined_rotated->Draw("colz") ;
+	x_mm_y_mm_near_left_aligned_combined_rotated_profile->Draw("same") ;
+
+	c.SaveAs("plots/ElasticAlignment/x_mm_y_mm_near_left_aligned_combined_rotated_profile.root") ;
+	c.SaveAs("plots/ElasticAlignment/x_mm_y_mm_near_left_aligned_combined_rotated_profile.pdf") ;
+	
+	
+	
+	
+	TH1D *px1 = x_mm_y_mm_near_left_aligned_combined->ProjectionX("px1", 600, 660) ;
+	TH1D *px2 = x_mm_y_mm_near_left_aligned_combined->ProjectionX("px2", 366, 425) ;
+
+
+	px1->Draw() ;
+	px1->GetXaxis()->SetRangeUser(-10.0, 10.0) ;
+	px2->Draw("same") ;
+
+	c.SaveAs("plots/ElasticAlignment/px.root") ;
+	c.SaveAs("plots/ElasticAlignment/px.pdf") ;
+
+}
+*/
+
+
+TFile *file_LBRT = NULL ;
+TFile *file_LTRB = NULL ;
+
+void combine_and_fit(string name, double low, double high, bool swap = false)
+{
+	TCanvas *c = new TCanvas() ;
+
+	TH2D *LBRT = ((TH2D *)(file_LBRT->Get(name.c_str()))) ;
+	TH2D *LTRB = ((TH2D *)(file_LTRB->Get(name.c_str()))) ;
+	
+	c->SetTitle(LBRT->GetName()) ;
+	cout << "myname " << LBRT->GetName() << endl ;
+	c->SetTitle("") ;
+	
+	// LBRT->SaveAs("delete_it.root") ;
+
+	TH1D *px1 = NULL ;
+	TH1D *px2 = NULL ;
+	
+	if(swap)
+	{
+		px1 = LBRT->ProjectionX("px1", 600, 660) ;
+		px2 = LTRB->ProjectionX("px2", 366, 425) ;	
+	}
+	else
+	{
+		px1 = LTRB->ProjectionX("px1", 600, 660) ;
+		px2 = LBRT->ProjectionX("px2", 366, 425) ;	
+	}
+	
+	double px1_mean = px1->GetMean() ;
+	double px2_mean = px2->GetMean() ;
+
+	TFitResultPtr ptr1 = px1->Fit("gaus","S", "", px1_mean + low, px1_mean + high) ;
+	TFitResultPtr ptr2 = px2->Fit("gaus","S", "", px2_mean + low, px2_mean + high) ;
+	
+	double mean_1 = ptr1->Parameter(1) ;
+	double mean_2 = ptr2->Parameter(1) ;
+
+	double avg = (mean_1 + mean_2) / 2.0 ;
+	double var = fabs(avg - mean_1) ;
+	
+	cout << "fit_result mean: " << mean_1 << endl ;
+	cout << "fit_result mean: " << mean_2 << endl ;
+	cout << "fit_result avg: " << avg << endl ;
+	cout << "fit_result var: " << var << endl ;
+	cout << "fit_result" << endl ;
+
+	px1->Draw() ;
+	px1->GetXaxis()->SetRangeUser(-10.0, 10.0) ;
+	px2->Draw("same") ;
+
+	c->SaveAs(("plots/ElasticAlignment/proj_" + name + ".root").c_str()) ;
+	c->SaveAs(("plots/ElasticAlignment/proj_" + name + ".pdf").c_str()) ;
+	
+	delete px1 ;
+	delete px2 ;
+	
+	delete c ;
+
+}
+
+main()
+{
+	gStyle->SetLineScalePS(.3) ;
+	gStyle->SetOptFit(1111);
+
+	file_LBRT = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts/Generic.root") ;
+	file_LTRB = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts/Generic.root") ;
+
+	combine_and_fit("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned", -2.0, 2.0) ;
+	combine_and_fit("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned", -1.5, 1.5) ;
+	combine_and_fit("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned", -2.0, 2.0, true) ;
+	combine_and_fit("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned", -1.5, 1.5, true) ;
+}
