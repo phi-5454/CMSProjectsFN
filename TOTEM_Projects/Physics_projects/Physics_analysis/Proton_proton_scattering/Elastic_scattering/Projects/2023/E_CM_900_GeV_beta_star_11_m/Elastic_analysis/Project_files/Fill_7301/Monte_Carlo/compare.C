099e22e6c4f970c636dd4435ad9a6a0ceaba9653
Frici <fnemes@cern.ch>
2023-11-02 10:17:21 +0100

    Compare

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_11_m/Elastic_analysis/Project_files/Fill_7301/Monte_Carlo/compare.C
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_11_m/Elastic_analysis/Project_files/Fill_7301/Monte_Carlo/compare.C
@@ -5,8 +5,19 @@ void compare()
 	
 	int scenario = scenario_1_no_beam_divergence_no_vertex_single_mc ;
 	
+	string filename1 = "" ;
+	string filename2 = "" ;
+	
+	string histname1 = "" ;
+	string histname2 = "" ;
+	
 	if(scenario == scenario_1_no_beam_divergence_no_vertex_single_mc)
 	{
+		filename1 = "/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test_hor_cor_mc_reference/Generic.root"" ;
+		filename2 = "/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test_hor_cor_mc/Generic.root" ;
+		
+		histname1= "P0025_PlotsCollection_dN_dt_GeV2" ;
+		histname2= "P0031_PlotsCollection_dN_dt_GeV2_geometrical_acceptance_corrected" ;
 	}
 	else
 	{
@@ -15,11 +26,11 @@ void compare()
 	}
 
 
-	TFile *myfile_reference = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test_hor_cor_mc_reference/Generic.root") ;
-	TFile *myfile_accep_corrected = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test_hor_cor_mc/Generic.root") ;
+	TFile *myfile_reference = TFile::Open(filename1.c_str()) ;
+	TFile *myfile_accep_corrected = TFile::Open(filename2.c_str()) ;
 	
-	TH1D *reference = (TH1D *)myfile_reference->Get("P0025_PlotsCollection_dN_dt_GeV2") ;
-	TH1D *accep_corrected = (TH1D *)myfile_accep_corrected->Get("P0031_PlotsCollection_dN_dt_GeV2_geometrical_acceptance_corrected") ;
+	TH1D *reference = (TH1D *)myfile_reference->Get(histname1.c_str()) ;
+	TH1D *accep_corrected = (TH1D *)myfile_accep_corrected->Get(histname2.c_str()) ;
 	
 	cout << reference << endl ;
 	cout << accep_corrected << endl ;
