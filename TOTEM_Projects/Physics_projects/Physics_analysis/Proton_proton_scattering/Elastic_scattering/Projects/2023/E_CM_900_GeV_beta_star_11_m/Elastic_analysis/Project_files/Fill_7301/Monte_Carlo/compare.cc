9eb96ba396543db911c02b8611a89720c1398129
Frici <fnemes@cern.ch>
2023-11-03 09:52:37 +0100

    MC improvements

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_11_m/Elastic_analysis/Project_files/Fill_7301/Monte_Carlo/compare.cc
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_11_m/Elastic_analysis/Project_files/Fill_7301/Monte_Carlo/compare.cc
@@ -27,8 +27,64 @@ using namespace std;
 #include "TStyle.h"
 #include "TFitResult.h"
 
+void condor()
+{
+	string file1 = "/eos/cms/store/user/fnemes/Analysis/pp/E_CM_900_GeV_beta_star_11_m/Monte_Carlo/Condor/Generic_MC_reference.root" ;
+	string file2 = "/eos/cms/store/user/fnemes/Analysis/pp/E_CM_900_GeV_beta_star_11_m/Monte_Carlo/Condor/Generic_MC_reference_with_beam_divergence.root" ;
+	string file3 = "/eos/cms/store/user/fnemes/Analysis/pp/E_CM_900_GeV_beta_star_11_m/Monte_Carlo/Condor/Generic_MC.root" ;
+	string file4 = "/eos/cms/store/user/fnemes/Analysis/pp/E_CM_900_GeV_beta_star_11_m/Monte_Carlo/Condor/Generic_MC_with_beam_divergence.root" ;
+	string file5 = "/eos/cms/store/user/fnemes/Analysis/pp/E_CM_900_GeV_beta_star_11_m/Monte_Carlo/Condor/Generic_MC_with_beam_divergence_stronger_cuts.root" ;
+	
+	TFile *file_1 = TFile::Open(file1.c_str()) ;
+	TFile *file_2 = TFile::Open(file2.c_str()) ;
+	TFile *file_3 = TFile::Open(file3.c_str()) ;
+	TFile *file_4 = TFile::Open(file4.c_str()) ;
+	TFile *file_5 = TFile::Open(file5.c_str()) ;
+	
+	TH2D *hist_1 = (TH2D *)file_1->Get("P0042_PlotsCollection_phi_star_rad_theta_star_rad") ;
+	TH2D *hist_2 = (TH2D *)file_2->Get("P0042_PlotsCollection_phi_star_rad_theta_star_rad") ;
+	TH2D *hist_3 = (TH2D *)file_3->Get("P0046_PlotsCollection_phi_star_rad_theta_star_rad") ;
+	TH2D *hist_4 = (TH2D *)file_4->Get("P0046_PlotsCollection_phi_star_rad_theta_star_rad") ;
+	TH2D *hist_5 = (TH2D *)file_5->Get("P0046_PlotsCollection_phi_star_rad_theta_star_rad") ;
+	
+	cout <<  hist_1 << " " <<  hist_2 << " " <<  hist_3 << " " <<  hist_4 << " " <<  hist_5 << endl ;
+	
+	hist_1->GetXaxis()->SetRangeUser(-2, 8) ;
+	hist_2->GetXaxis()->SetRangeUser(-2, 8) ;
+	hist_3->GetXaxis()->SetRangeUser(-2, 8) ;
+	hist_4->GetXaxis()->SetRangeUser(-2, 8) ;
+	hist_5->GetXaxis()->SetRangeUser(-2, 8) ;
+
+	hist_1->GetYaxis()->SetRangeUser(-5e-4, 15e-4) ;
+	hist_2->GetYaxis()->SetRangeUser(-5e-4, 15e-4) ;
+	hist_3->GetYaxis()->SetRangeUser(-5e-4, 15e-4) ;
+	hist_4->GetYaxis()->SetRangeUser(-5e-4, 15e-4) ;
+	hist_5->GetYaxis()->SetRangeUser(-5e-4, 15e-4) ;
+	
+	TCanvas c2 ;
+   c2.SetLogz() ;
+	
+	hist_1->Draw("colz") ;
+	c2.SaveAs("plots/condor/hist_1.pdf") ;
+
+	hist_2->Draw("colz") ;
+	c2.SaveAs("plots/condor/hist_2.pdf") ;
+
+	hist_3->Draw("colz") ;
+	c2.SaveAs("plots/condor/hist_3.pdf") ;
+
+	hist_4->Draw("colz") ;
+	c2.SaveAs("plots/condor/hist_4.pdf") ;
+
+	hist_5->Draw("colz") ;	
+	c2.SaveAs("plots/condor/hist_5.pdf") ;
+
+}
+
 int main()
 {
+	condor() ;
+
 	const int test_of_vertical_acceptance_cut_without_divergence = 1 ;
 	const int test_of_vertical_acceptance_cut_with_divergence = 2 ;
 	
