8971a228ebb648984da080996862b72295bc9f93
Frici <fnemes@cern.ch>
2023-11-13 17:47:33 +0100

    ok

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/ElasticAlignment.cc
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/ElasticAlignment.cc
@@ -118,11 +118,13 @@ void combine_and_fit(string name, double low, double high, bool swap = false)
 
 }
 
-void combine_and_fit_y(string name, double low, double high, int bins)
+void combine_and_fit_y(string name, double low, double high, int bins, int fill)
 {
 	TCanvas *c = new TCanvas() ;
 	
-	ifstream acceptance_cuts("data/acceptance_cuts_for_vertical_alignment/acceptance_cuts_for_vertical_alignment.txt") ;
+	string filename = "data/acceptance_cuts_for_vertical_alignment/acceptance_cuts_for_vertical_alignment.txt" ;
+	
+	ifstream acceptance_cuts(filename.c_str()) ;
 	
 	double LBRT_cut_y_mm_low = 0 ;
 	double LBRT_cut_y_mm_high = 0 ;
@@ -212,8 +214,10 @@ void combine_and_fit_y(string name, double low, double high, int bins)
 	TH2D *LBRT = ((TH2D *)(file_LBRT->Get(name.c_str()))) ;
 	TH2D *LTRB = ((TH2D *)(file_LTRB->Get(name.c_str()))) ;
 	
-	TH2D *combined = (TH2D *)LBRT->Clone("clone") ;
+	TH2D *combined = (TH2D *)LBRT->Clone(name.c_str()) ;
 	combined->Add(LTRB) ;
+	
+	combined->SaveAs(("plots/ElasticAlignment/combined_" + name + ".root").c_str()) ;
 
 	cout << "vertmyname " << LBRT->GetName() << endl ;
 
@@ -221,6 +225,13 @@ void combine_and_fit_y(string name, double low, double high, int bins)
 	TH1D *py2 = NULL ;
 	TH1D *py3 = NULL ;
 	
+	int bin1 = combined->GetXaxis()->FindBin(-2.0) ;
+	int bin2 = combined->GetXaxis()->FindBin( 2.0) ;
+	cout << "bin1 " << bin1 << endl ;
+	cout << "bin2 " << bin2 << endl ;
+	
+	// py1 = combined->ProjectionY("py1", bin1, bin2) ; 
+	// py1 = combined->ProjectionY("py1", bin1, bin2) ; 
 	py1 = combined->ProjectionY("py1") ; 
 	py2 = LBRT->ProjectionY("py2") ; 
 	py3 = LTRB->ProjectionY("py3") ; 
@@ -230,6 +241,9 @@ void combine_and_fit_y(string name, double low, double high, int bins)
 	//py2->Draw() ;
 	//py3->Draw("same") ;
 	py1->Draw("") ;
+	
+	py1->SaveAs(("plots/ElasticAlignment/projy_" + name + "_hist_before.root").c_str()) ;
+	
 	// py1->GetXaxis()->SetRangeUser(-30.0, 30.0) ;
 	
 	int nbins = py1->GetXaxis()->GetNbins() ;
@@ -242,15 +256,22 @@ void combine_and_fit_y(string name, double low, double high, int bins)
 		if((LBRT_cut_y_mm_low < 0) && ((LBRT_cut_y_mm_high > biny) || ((biny > LBRT_cut_y_mm_low) && (biny < LTRB_cut_y_mm_low)) || (LTRB_cut_y_mm_high < biny)))
 		{
 			// cout << "here I am2" << endl ;
-			py1->SetBinContent(i, 0) ;
-			py1->SetBinError(i, 0) ;
+			// if(fill == 7301)
+			{
+				py1->SetBinContent(i, 0) ;
+				py1->SetBinError(i, 0) ;
+			}
 		}
 		
 		if((LBRT_cut_y_mm_low > 0) && ((LTRB_cut_y_mm_high > biny) || ((biny > LTRB_cut_y_mm_low) && (biny < LBRT_cut_y_mm_low)) || (LBRT_cut_y_mm_high < biny)))
 		{
 			cout << "here I am" << endl ;
-			py1->SetBinContent(i, 0) ;
-			py1->SetBinError(i, 0) ;
+
+			// if(fill == 7301)
+			{
+				py1->SetBinContent(i, 0) ;
+				py1->SetBinError(i, 0) ;
+			}
 		}
 
 
@@ -262,7 +283,10 @@ void combine_and_fit_y(string name, double low, double high, int bins)
 	py1->GetFunction("gaus")->SetLineColor(kBlue) ;
 	double mean = ptr->Parameter(1) ;
 	cout << "fit_resulty mean: " << name << " " << mean << endl ;
+	
+	py1->SetLineColor(kBlue) ;
 
+	py1->SaveAs(("plots/ElasticAlignment/projy_" + name + "_hist.root").c_str()) ;
 	c->SaveAs(("plots/ElasticAlignment/projy_" + name + ".root").c_str()) ;
 	c->SaveAs(("plots/ElasticAlignment/projy_" + name + ".pdf").c_str()) ;
 
@@ -279,19 +303,33 @@ main()
 
 	// file_LBRT = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts/Generic.root") ;
 	// file_LTRB = TFile::Open("/afs/cern.ch/work/f/fnemes/tmp/pp/E_CM_900_GeV_beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts/Generic.root") ;
+	
+	int fill = 7302 ;
 
-//	file_LBRT = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
-//	file_LTRB = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
-	file_LBRT = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7302/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
-	file_LTRB = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7302/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
+	if(fill == 7301)
+	{
+		file_LBRT = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
+		file_LTRB = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7301/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
+
+		combine_and_fit_y("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned", -10.0, 10.0, 90, fill) ;
+		combine_and_fit_y("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned", -10.0, 10.0,  90, fill) ;
+		combine_and_fit_y("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned", -9.0, 9.0,  70, fill) ;
+		combine_and_fit_y("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned",  -9.0, 9.0,  70, fill) ;
+	}
+	else if(fill == 7302)
+	{
+		file_LBRT = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7302/Diagonals/DIAGONAL_LEFT_BOTTOM_RIGHT_TOP/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
+		file_LTRB = TFile::Open("/eos/cms/store/group/phys_diffraction/fnemes/E_CM_900_GeV/Beta_star_11_m/Analysis_output_files/7302/Diagonals/DIAGONAL_LEFT_TOP_RIGHT_BOTTOM/All_root_files_to_define_cuts_aligned_first_optics_test/Generic.root") ;
+
+		combine_and_fit_y("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned", -10.0, 10.0, 90, fill) ;
+		combine_and_fit_y("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned", -10.0, 10.0,  90, fill) ;
+		combine_and_fit_y("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned", -9.0, 9.0,  70, fill) ;
+		combine_and_fit_y("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned",  -9.0, 9.0,  70, fill) ;
+	}
+
 
 	// combine_and_fit("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned", -2.0, 2.0) ;
 	// combine_and_fit("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned", -1.5, 1.5) ;
 	// combine_and_fit("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned", -2.0, 2.0, true) ;
 	// combine_and_fit("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned", -1.5, 1.5, true) ;
-
-	combine_and_fit_y("P0079_PlotsCollection_x_mm_y_mm_near_left_aligned", -10.0, 10.0, 90) ;
-	combine_and_fit_y("P0080_PlotsCollection_x_mm_y_mm_far_left_aligned", -10.0, 10.0,  90) ;
-	combine_and_fit_y("P0081_PlotsCollection_x_mm_y_mm_near_right_aligned", -9.0, 9.0,  70) ;
-	combine_and_fit_y("P0082_PlotsCollection_x_mm_y_mm_far_right_aligned",  -9.0, 9.0,  70) ;
 }
