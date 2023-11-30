cc3b9804d8785d46214252aaedd00e2442e98131
Frici <fnemes@cern.ch>
2023-11-30 15:29:51 +0100

    For alignment and optics uncertainty

---
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_11_m/Elastic_analysis/Project_files/Fill_7301/Monte_Carlo/Condor/MC_reference_with_beam_divergence_exp/run_mine.sh
@@ -0,0 +1,61 @@
+cd dir_0000
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0001
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0002
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0003
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0004
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0005
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0006
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0007
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0008
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0009
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0010
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0011
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0012
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0013
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0014
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0015
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0016
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0017
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0018
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ../dir_0019
+rm *.log
+../../../../../../../../../../analyse cfg.prj &> /dev/null &
+cd ..
