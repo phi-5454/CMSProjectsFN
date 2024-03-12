9e724cfbf9c28aae8a1a0c43c34da61fde955df5
Frici <fnemes@cern.ch>
2024-03-12 15:08:19 +0100

    Ok

--- Cross_section_figures_TOTEM/2024_03_12_plots_for_long_paper/plot.cc
+++ Cross_section_figures_TOTEM/2024_03_12_plots_for_long_paper/plot.cc
@@ -21,7 +21,7 @@ using namespace std;
 #include "TLegend.h"
 #include "TCanvas.h"
 #include "TAxis.h"
-#include "TGraphAsymmErrors.h"
+#include "TGraphErrors.h"
 #include "TLatex.h"
 #include "TLine.h"
 #include "TMinuit.h"
@@ -37,7 +37,79 @@ using namespace std;
 
 #include <iomanip>
 
+Double_t log_like_function(Double_t *x, Double_t *par)
+{
+        double f1 = par[0] * pow(log(x[0]), 0) ;
+        double f2 = par[1] * pow(log(x[0]), 1) ;
+        double f3 = par[2] * pow(log(x[0]), 2) ;
+
+        double f = f1 + f2 + f3 ;
+      
+      // cout << "energy: " << x[0] << " " << f << endl ;
+
+        return f ;
+}
+
+
 int main(int argc, char *argv[])
 {
+   gStyle->SetLineScalePS(.3) ;
+	gStyle->SetOptFit(1111);
+	gStyle->SetOptStat("");
 
+	TGraphErrors *graph = new TGraphErrors ;
+	TGraphErrors *graph_1p96 = new TGraphErrors ;
+	
+	TCanvas c ;
+	
+	TH2D *hist_2d = new TH2D("hist_2d", "hist_2d", 100, 1, 14, 100, 75, 115) ;	
+	hist_2d->SetTitle("") ;
+
+	graph->SetMarkerStyle(20) ;
+	graph_1p96->SetMarkerStyle(20) ;
+
+	graph_1p96->SetMarkerColor(kBlue) ;
+	
+	ifstream data("data/TOTEM_D0_14_PRL_preliminary_1_cross_section.txt") ;
+	
+	string word ;
+	
+	double energy, sigtot, sigtotunc ;
+
+	int i = 0 ;
+	
+	while(data >> energy >> sigtot >> sigtotunc)
+	{
+		cout << energy << endl ;
+
+		if(energy == 1.96)
+		{
+			graph_1p96->SetPoint(0, energy, sigtot) ;
+			graph_1p96->SetPointError(0, 0, sigtotunc) ;
+		}
+		else
+		{
+			graph->SetPoint(i, energy, sigtot) ;
+			graph->SetPointError(i, 0, sigtotunc) ;
+			++i ;
+		}
+	}
+
+   double epsilon = 0.01 ;
+	TF1 *func = new TF1("func",  log_like_function, epsilon, 14, 3) ;	
+	
+	
+   func->SetParameters(73.75607, 8.41508, 2.29419) ;
+   func->SetNpx(100) ;	
+
+	hist_2d->Draw() ;
+	hist_2d->GetXaxis()->SetTitle("#sqrt{s} (TeV)") ;
+	hist_2d->GetYaxis()->SetTitle("#sigma_{tot} (mb)") ;
+
+	graph->Draw("same p") ;
+	graph_1p96->Draw("same p") ;
+	func->SetRange(1.5, 15) ;
+	func->Draw("same l") ;
+
+	c.SaveAs("fig/TOTEM_D0_14_PRL_preliminary_1_cross_section.pdf") ;
 }
