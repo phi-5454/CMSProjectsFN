13e01dfcbd3e741cdb4906c06bb90e158d75554a
Frici <fnemes@cern.ch>
2024-03-21 16:40:24 +0100

    Ok

---
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/HB/project_files_per_run.sh
@@ -0,0 +1,1 @@
+for i in `cat ../../../../E_CM_900_GeV_beta_star_100_m/General_settings/List_of_runs.txt` ; do cat ../../Project_files/Per_run/Left_bottom_right_top_4_sigma_all_root_files_to_define_cuts_template.prj | sed "s/NNNN/$i/g" > ../../Project_files/Per_run/Left_bottom_right_top_4_sigma_all_root_files_to_define_cuts_run_$i.prj ; done
