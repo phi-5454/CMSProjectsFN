011c30144305234c6676418c310127cc70b2326e
Frici <fnemes@cern.ch>
2024-03-27 13:15:27 +0100

    Ok

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/run_all_LTRB.sh
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/run_all_LTRB.sh
@@ -1,37 +1,37 @@
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324457$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324458$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324460$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324461$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324457$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324458$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324460$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324461$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324462$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324464$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324465$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324467$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324462$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324464$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324465$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324467$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324469$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324471$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324485$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324498$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324469$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324471$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324485$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324498$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324509$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324513$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324517$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324518$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324509$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324513$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324517$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324518$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324519$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324520$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324524$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324526$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324519$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324520$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324524$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324526$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324527$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324528$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324529$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324530$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324527$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324528$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324529$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324530$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324531$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324532$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324534$1.prj >> /dev/null &
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324535$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324531$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324532$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324534$1.prj &> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324535$1.prj &> /dev/null &
 wait
-./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324536$1.prj >> /dev/null &
+./analyse Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Project_files/Per_run/Left_top_right_bottom_4_sigma_all_root_files_to_define_cuts_run_324536$1.prj &> /dev/null &
 
