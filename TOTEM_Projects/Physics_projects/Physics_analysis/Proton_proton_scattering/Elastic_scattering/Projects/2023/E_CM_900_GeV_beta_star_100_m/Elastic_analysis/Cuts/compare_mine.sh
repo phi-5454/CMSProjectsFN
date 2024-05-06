412b716d102bc23c9090a1648cf9e0d14431729d
Frici <fnemes@cern.ch>
2024-05-06 16:38:00 +0200

    LTRB first try

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/compare_mine.sh
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/compare_mine.sh
@@ -1,2 +1,4 @@
-for i in `ls  -c1 *.prj` ; do diff $i Temp/$i ; done
-
+echo LBRT
+for i in `ls  -c1 *LBRT*.prj` ; do diff $i Temp/$i ; done
+echo LTRB
+for i in `ls  -c1 *LTRB*.prj` ; do diff $i Temp/$i ; done
