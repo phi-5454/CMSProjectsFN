95b646a5eba3af214676d64a3b5621d897fbefe4
Frici <fnemes@cern.ch>
2024-05-08 09:49:31 +0200

    Close to final alignment

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/compare_mine.sh
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/compare_mine.sh
@@ -1,4 +1,2 @@
-echo LBRT
-for i in `ls  -c1 *LBRT*.prj` ; do diff $i Temp/$i ; done
-echo LTRB
-for i in `ls  -c1 *LTRB*.prj` ; do diff $i Temp/$i ; done
+for i in `ls  -c1 *.prj` ; do diff $i Temp/$i ; done
+
