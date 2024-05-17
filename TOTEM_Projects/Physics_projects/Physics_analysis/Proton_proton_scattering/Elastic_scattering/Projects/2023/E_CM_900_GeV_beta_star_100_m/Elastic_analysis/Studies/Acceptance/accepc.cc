d078f1e67c070eca4ff46b401d16de86a3836479
Frici <fnemes@cern.ch>
2024-05-17 10:20:06 +0200

    Write only if needed

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Acceptance/accepc.cc
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Acceptance/accepc.cc
@@ -518,25 +518,31 @@ void write_correction(string run_to_test, map<string, double> &correction)
 	{
 		string corr_filename = "vertical_corrections_" + run_to_test + ".prj" ;
 
+		ifstream correction_file ;
+
+
 		ofstream correction_file ;
 
-		bool write_vertical_file = true ;
+		bool write_vertical_file = false ;
 
-		if(write_vertical_file) correction_file.open("/afs/cern.ch/work/f/fnemes/main_workspace_github_ssh_4/Projects/TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/Vertical/" + corr_filename) ;
+		if(write_vertical_file)
+		{
+			correction_file.open("/afs/cern.ch/work/f/fnemes/main_workspace_github_ssh_4/Projects/TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Cuts/Vertical/" + corr_filename) ;
 
 
-		correction_file << "<begin_if_setting> vertical_alignment_corrections yes" << endl ;
+			correction_file << "<begin_if_setting> vertical_alignment_corrections yes" << endl ;
 
-		correction_file << "  <begin> alignment_block" << endl ;
-		correction_file << "                  <update_parameter> RP_alignment_left_near_y_mm "  << -correction["P0025_PlotsCollection_x_mm_y_mm_near_left_for_2RP"] << endl ;
-		correction_file << "                  <update_parameter> RP_alignment_left_far__y_mm "   << -correction["P0026_PlotsCollection_x_mm_y_mm_far_left_for_2RP"] << endl ;
-		correction_file << "                  <update_parameter> RP_alignment_right_near_y_mm " << -correction["P0027_PlotsCollection_x_mm_y_mm_near_right_for_2RP"] << endl ;
-		correction_file << "                  <update_parameter> RP_alignment_right_far__y_mm "  << -correction["P0028_PlotsCollection_x_mm_y_mm_far_right_for_2RP"] << endl ;
-		correction_file << "  <end> alignment_block" << endl ;
+			correction_file << "  <begin> alignment_block" << endl ;
+			correction_file << "                  <update_parameter> RP_alignment_left_near_y_mm "  << -correction["P0025_PlotsCollection_x_mm_y_mm_near_left_for_2RP"] << endl ;
+			correction_file << "                  <update_parameter> RP_alignment_left_far__y_mm "   << -correction["P0026_PlotsCollection_x_mm_y_mm_far_left_for_2RP"] << endl ;
+			correction_file << "                  <update_parameter> RP_alignment_right_near_y_mm " << -correction["P0027_PlotsCollection_x_mm_y_mm_near_right_for_2RP"] << endl ;
+			correction_file << "                  <update_parameter> RP_alignment_right_far__y_mm "  << -correction["P0028_PlotsCollection_x_mm_y_mm_far_right_for_2RP"] << endl ;
+			correction_file << "  <end> alignment_block" << endl ;
 
-		correction_file << "<end_if_setting>" << endl ;
+			correction_file << "<end_if_setting>" << endl ;
 
-		correction_file.close() ;
+			correction_file.close() ;
+		}
 	}
 }
 
