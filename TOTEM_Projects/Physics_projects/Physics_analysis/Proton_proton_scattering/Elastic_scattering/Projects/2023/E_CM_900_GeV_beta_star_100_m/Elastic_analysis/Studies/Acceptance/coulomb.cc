873a42d41fd6c2d21a5718cf76b51ce65a5f3289
Frici <fnemes@cern.ch>
2024-05-23 14:09:51 +0200

    hbarc

--- TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Acceptance/coulomb.cc
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Acceptance/coulomb.cc
@@ -42,49 +42,50 @@ using namespace std;
 
 #include <iomanip>
 
+const double hbarc = 0.19732697 ;                            // [hbarc] = GeV * fm
+
 const double Lambda2_GeV2 = 0.71 ;
 const double Fine_structure_constant = 1/137.035 ;
 
 double G_proton(double t)
 {
-    return 1 / pow(1-(-t/Lambda2_GeV2),2) ;
+  return 1 / pow(1-(-t/Lambda2_GeV2),2) ;
 }
 
 double Coulomb_amplitude(double t)
 {
-    return 2*sqrt(TMath::Pi())*(Fine_structure_constant)*(1/abs(t))*pow(G_proton(t),2) ;
+  return 2*sqrt(TMath::Pi())*(Fine_structure_constant)*(1/abs(t))*pow(G_proton(t),2) ;
 }
-
 
 double sigma(double *t, double *par)
 {
-    double ca = Coulomb_amplitude(t[0]) * pow(0.1973,2) * 2*sqrt(TMath::Pi()); // to have the same dimension, normalization, as in BB
-    double result = (10. * (pow(abs(ca),2) / pow(0.1973,2)))  / (4*TMath::Pi()) ;
+  double ca = Coulomb_amplitude(t[0]) * pow(hbarc, 2) * 2*sqrt(TMath::Pi()); // to have the same dimension, normalization, as in BB
+  double result = (10. * (pow(abs(ca),2) / pow(hbarc,2)))  / (4*TMath::Pi()) ;
 
-    cout << "test at t=" << (*t) << "\t " << result << endl ;
-
-	 return result ;
+  return result ;
 }
 
-
-int main()
+int main(int argc, char *argv[])
 {
-	double t_value = 0.02 ;
 
-	cout << sigma(&t_value, NULL) << endl ;
+  if(argc != 2) 
+  {
+    cout << "Please provide ..." << endl ;
+    exit(1) ;
+  }
+   const double epsilon = 1e-5 ;
 
-	double epsilon = 1e-5 ;
+  TF1 *func = new TF1("sigma",  sigma, epsilon, 1, 0) ;
+  func->SetNpx(1e4) ;
 
-	TF1 *func = new TF1("sigma",  sigma, epsilon, 1, 0) ;
-	func->SetNpx(1e4) ;
+  TCanvas c ;
 
-	TCanvas c ;
+  TH2D *hist_2d = new TH2D("hist_2d", "hist_2d", 100, 0, 1, 100, 0, 5) ;
+  hist_2d->SetTitle("") ;
+  hist_2d->Draw() ;
 
-   TH2D *hist_2d = new TH2D("hist_2d", "hist_2d", 100, 0, 1, 100, 0, 5) ;
-	hist_2d->SetTitle("") ;
-	hist_2d->Draw() ;
-
-	func->Draw("same l") ;
-	func->SaveAs("plots/coulomb/func_coulomb_test.root") ;
-	c.SaveAs("plots/coulomb/canvas_coulomb_test.root") ;
+  func->Draw("same l") ;
+  func->SaveAs("plots/coulomb/func_coulomb_test.root") ;
+  c.SaveAs("plots/coulomb/canvas_coulomb_test.root") ;
+  c.SaveAs("plots/coulomb/canvas_coulomb_test.pdf") ;
 }
