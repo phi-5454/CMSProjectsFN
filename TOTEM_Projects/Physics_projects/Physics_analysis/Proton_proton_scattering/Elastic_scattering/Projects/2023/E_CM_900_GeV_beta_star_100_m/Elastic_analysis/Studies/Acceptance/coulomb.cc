7917d8c733ca24365cf43ebe72f2934c50c0cdda
Frici <fnemes@cern.ch>
2024-05-23 12:46:12 +0200

    Fit test

---
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Acceptance/coulomb.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "math.h"
+#include "math.h"
+
+using namespace std;
+
+#include "stdlib.h"
+
+#include "TMath.h"
+
+#include "TF1.h"
+#include "TH2D.h"
+#include "TPave.h"
+#include "TBox.h"
+#include "TGraph.h"
+#include "TFile.h"
+#include "TH1D.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TGraphAsymmErrors.h"
+#include "TLatex.h"
+#include "TLine.h"
+#include "TMinuit.h"
+#include "TStyle.h"
+
+#include "TVector.h"
+#include "TMatrix.h"
+#include "TMarker.h"
+#include "TFitResult.h"
+
+#include "TRandom3.h"
+#include "TEllipse.h"
+#include "TMultiGraph.h"
+
+#include <fstream>
+
+#include <iomanip>
+
+int main()
+{
+}
