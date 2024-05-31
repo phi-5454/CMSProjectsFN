16c2a7deab7eca56a51be33ff290ab58a6053f29
Frici <fnemes@cern.ch>
2024-05-31 08:42:55 +0200

    Condor

---
+++ TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Monte_Carlo/Condor/MC_reference/dir_0000/cfg_analyzer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/f/fnemes/main_workspace_github_ssh_4/Projects/TOTEM_Projects/Physics_projects/Physics_analysis/Proton_proton_scattering/Elastic_scattering/Projects/2023/E_CM_900_GeV_beta_star_100_m/Elastic_analysis/Studies/Monte_Carlo/Condor/MC_reference/dir_0000/
+../../../../../../../../../analyse cfg.prj
